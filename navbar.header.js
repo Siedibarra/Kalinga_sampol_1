29fd27319d590de61ce6d43e9a8532e990408f3b
Siedibarra <siedjeremiahibarra@gmail.com>
2023-03-22 16:04:28 +0800

    modified:   carousel_file.blade.html 	modified:   header.blade.html 	modified:   navbar.header.js 	modified:   navbar.style.css 	modified:   sample2.html

--- navbar.header.js
+++ navbar.header.js
@@ -1,11 +1,29 @@
 
-var btnContainer = document.getElementById("navbar-nav");
-var btns = btnContainer.getElementByClassName("nav-link");
+// var btnContainer = document.getElementById("navbar-nav");
+// var btns = btnContainer.getElementByClassName("nav-link");
 
-for(var i = 0; i<btns.length; i++) {
-    btns[i].addEventListener('click', function(){
-        var current = document.getElementByClassName("active");
-        current[0].className = current[0].className.replace(" active");
-        this.className += " active";
-    })
+// for(var i = 0; i<btns.length; i++) {
+//     btns[i].addEventListener('click', function(){
+//         var current = document.getElementByClassName("active");
+//         current[0].className = current[0].className.replace(" active");
+//         this.className += " active";
+//     })
+// }
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".nav-item a");
+const menuLength = menuItem.length
+for(let i = 0; i<menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = "active"
+    }
 }
+
+
+// let navlinks = document.querySelectorAll(".nav-item a");
+// let currentURL = window.location.href;
+// for (let i = 0; i<navlinks/length; i++) {
+//     if (navlinks[i].href === currentURL) {
+//         navlinks[i].className = ("active");
+//     }
+// }
